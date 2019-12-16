bf5887389d7039c825c7233fb44ca3cf172641ae
Jayson Grace <jayson.e.grace@gmail.com>
2019-12-15 21:48:32 -0800

    Punctuation fixes

--- pkg/moseutils/netutils.go
+++ pkg/moseutils/netutils.go
@@ -63,7 +63,7 @@ func GetLocalIP() (string, error) {
 			return ip.String(), nil
 		}
 	}
-	return "", errors.New("are you connected to the network?")
+	return "", errors.New("Are you connected to the network?")
 }
 
 func singleFile(h http.Handler) http.Handler {
